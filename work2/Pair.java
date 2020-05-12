93180af1fb39122baa484394996ebd78a55ca2ff
takfujii29 <takfujii29@icloud.com>
2020-05-12 09:33:46 +0900

    work2

--- work2/Pair.java
+++ work2/Pair.java
@@ -1,3 +1,25 @@
 public class Pair {
-  public
+  
+  public int first;
+  public int second;
+
+  Pair(int first, int second) {
+    this.first = first;
+    this.second = second;
+  }
+  public static void main(String[] args) {
+    Pair p = new Pair(1, 2);
+    int first = p.first();
+    int second = p.second();
+    System.out.println(first);
+    System.out.println(second);
+
+  }
+  public int first() {
+    return this.first;
+  }
+
+  public int second() {
+    return this.second;
+  }
 }
